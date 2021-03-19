e24adf943589ee3a48bb826f1b654900a101fb7f
asrmrc <31941055+asrmrc@users.noreply.github.com>
2021-03-18 21:30:23 -0700

    Add Number228

---
+++ Number228.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Number228 {
+	public List<String> summaryRanges(int[] nums){
+		List<String> result=new ArrayList<>();
+		for (int i=0; i<nums.length; i++) {
+			int num=nums[i];
+			while(i<nums.length-1 && nums[i]+1==nums[i+1]) {
+				i++;
+			}
+			
+			if(num!=nums[i]) {
+				result.add(num + "->" + nums[i]);
+	        } else {
+	            result.add(num + "");
+			}
+		}
+		return result;
+	}
+}
