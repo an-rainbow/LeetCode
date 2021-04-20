af85daa0f7fef19f294dcf631704fc563b84f918
Ann A <31941055+an-rainbow@users.noreply.github.com>
2021-04-20 00:25:06 -0700

    Add files via upload

---
+++ Number1460.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Number1460 {
+	
+	//O(nlogn)
+
+	public boolean canBeEqual(int[]target, int[]arr) {
+		Arrays.sort(target);
+		Arrays.sort(arr);
+		
+		for(int i=0; i<target.length; i++) {
+			if(target[i]!=arr[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
